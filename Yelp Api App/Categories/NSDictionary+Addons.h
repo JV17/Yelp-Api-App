228828887dfab350b4127d56cfe1d6e3953884e8
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-11-28 14:53:58 -0500

    added nsdictionary category helper function to extract dictionaries from array.

--- Yelp Api App/Categories/NSDictionary+Addons.h
+++ Yelp Api App/Categories/NSDictionary+Addons.h
@@ -1,5 +1,5 @@
 //
-//  NSDictionary+Error.h
+//  NSDictionary+Addons.h
 //  Yelp Api App
 //
 //  Created by Jorge Valbuena on 2015-11-28.
@@ -9,7 +9,25 @@
 #import <Foundation/Foundation.h>
 
 
-@interface NSDictionary (Error)
+@interface NSDictionary (Addons)
+
+#pragma mark - Response Dictionary
+
+/**
+ Loads a dictionary with specific prefix keys from a specfied array of dictionaries. This method will add a number base on each element from the Array dictionary.
+ 
+ @param keyPrefix
+    The prefix key to be use as the new dictionary keys E.g. business. Resulting in business1, business2...
+ @param arrayDictionary
+    The array containing dictionaries to be extracted to different keys.
+ 
+ @return
+    A dictionary with dictionaries and specifies keys.
+ */
++ (instancetype)responseDictionaryWithKeyPrefix:(NSString *)keyPrefix arrayDictionary:(NSArray *)arrayDictionary;
+
+
+#pragma mark - Error Dictionary
 
 /**
  Builds a dictionary for an @NSError with description, reason and suggestion.
--- Yelp Api App/Categories/NSDictionary+Addons.m
+++ Yelp Api App/Categories/NSDictionary+Addons.m
@@ -1,15 +1,39 @@
 //
-//  NSDictionary+Error.m
+//  NSDictionary+Addons.m
 //  Yelp Api App
 //
 //  Created by Jorge Valbuena on 2015-11-28.
 //  Copyright © 2015 Jorge Valbuena. All rights reserved.
 //
 
-#import "NSDictionary+Error.h"
+#import "NSDictionary+Addons.h"
 
 
-@implementation NSDictionary (Error)
+@implementation NSDictionary (Addons)
+
+#pragma mark - Response Dictionary
+
++ (instancetype)responseDictionaryWithKeyPrefix:(NSString *)keyPrefix arrayDictionary:(NSArray *)arrayDictionary
+{
+    if (!arrayDictionary.count || !keyPrefix.length)
+    {
+        return nil;
+    }
+    
+    NSMutableDictionary *responseDictionary = [[NSMutableDictionary alloc] initWithCapacity:arrayDictionary.count];
+    NSInteger index = 0;
+    
+    for (NSDictionary *dictionary in arrayDictionary)
+    {
+        [responseDictionary setObject:dictionary forKey:[NSString stringWithFormat:@"%@%ld", keyPrefix, (index + 1)]];
+        index++;
+    }
+    
+    return [responseDictionary copy];
+}
+
+
+#pragma mark - Error Dictionary
 
 + (instancetype)errorDictionaryWithDescription:(NSString *)description reason:(NSString *)reason suggestion:(NSString *)suggestion
 {
