aac6d5aaa97c672bbef0bd9092099ed3bc0bed47
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-11-28 13:35:23 -0500

    updating project.

---
+++ Yelp Api App/Categories/NSDictionary+Error.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Error.h
+//  Yelp Api App
+//
+//  Created by Jorge Valbuena on 2015-11-28.
+//  Copyright © 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Error)
+
+@end
---
+++ Yelp Api App/Categories/NSDictionary+Error.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Error.m
+//  Yelp Api App
+//
+//  Created by Jorge Valbuena on 2015-11-28.
+//  Copyright © 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import "NSDictionary+Error.h"
+
+@implementation NSDictionary (Error)
+
+@end
