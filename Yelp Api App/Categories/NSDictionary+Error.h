a44ac5f03937825d7ae7e9de2a7dbac96358d01b
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-11-28 13:35:47 -0500

    Added NSDictionary category to help building errors.

--- Yelp Api App/Categories/NSDictionary+Error.h
+++ Yelp Api App/Categories/NSDictionary+Error.h
@@ -8,6 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+
 @interface NSDictionary (Error)
+
+/**
+ Builds a dictionary for an @NSError with description, reason and suggestion.
+ 
+ @param description
+    The description of the error.
+ @param reason
+    The reason of the error.
+ @param suggestion
+    The suggestion of the error.
+ 
+ @return
+    The error dictionary with description, reason and suggestion.
+ */
++ (instancetype)errorDictionaryWithDescription:(NSString *)description reason:(NSString *)reason suggestion:(NSString *)suggestion;
 
 @end
--- Yelp Api App/Categories/NSDictionary+Error.m
+++ Yelp Api App/Categories/NSDictionary+Error.m
@@ -8,6 +8,14 @@
 
 #import "NSDictionary+Error.h"
 
+
 @implementation NSDictionary (Error)
+
++ (instancetype)errorDictionaryWithDescription:(NSString *)description reason:(NSString *)reason suggestion:(NSString *)suggestion
+{
+    return @{NSLocalizedDescriptionKey: NSLocalizedString(description, nil),
+             NSLocalizedFailureReasonErrorKey: NSLocalizedString(reason, nil),
+             NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString(suggestion, nil)};
+}
 
 @end
