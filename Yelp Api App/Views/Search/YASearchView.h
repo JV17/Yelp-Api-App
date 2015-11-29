7bf27263bb80da1ed0946e855ade52ece97382cb
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-11-28 21:16:29 -0500

    added search view.

---
+++ Yelp Api App/Views/Search/YASearchView.h
@@ -0,0 +1,16 @@
+//
+//  YASearchView.h
+//  Yelp Api App
+//
+//  Created by Jorge Valbuena on 2015-11-28.
+//  Copyright © 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface YASearchView : UIView
+
+
+
+@end
---
+++ Yelp Api App/Views/Search/YASearchView.m
@@ -0,0 +1,16 @@
+//
+//  YASearchView.m
+//  Yelp Api App
+//
+//  Created by Jorge Valbuena on 2015-11-28.
+//  Copyright © 2015 Jorge Valbuena. All rights reserved.
+//
+
+#import "YASearchView.h"
+
+
+@implementation YASearchView
+
+
+
+@end
